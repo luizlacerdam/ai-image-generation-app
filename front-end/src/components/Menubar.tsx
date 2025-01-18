aab7b10283406b6fc6a24d65e1d1e3366cb407a9
luizlacerdam <luizlacerdam.dev@gmail.com>
2025-01-18 15:13:59 -0500

    styling navbar and button

--- front-end/src/components/Menubar.tsx
+++ front-end/src/components/Menubar.tsx
@@ -3,8 +3,10 @@ import { Button } from "./ui/button";
 const MenuBar = () => {
 	return (
 		<nav className="flex flex-row justify-between items-center bg-[#1e1f2a] text-white p-4 shadow-md">
-			<div className="font-semibold">luiz's images gen</div>
-			<Button type="button">+ Create new post</Button>
+			<div className="font-bold text-xl">luiz's GemAI</div>
+			<Button className="bg-violet-500 hover:bg-violet-800" type="button">
+				+ Create new post
+			</Button>
 		</nav>
 	);
 };
