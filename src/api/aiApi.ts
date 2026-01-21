3d477f89206352501d7d45075c1261606510b6ee
Luiz Melo <luizlacerdam.dev@gmail.com>
2026-01-20 20:19:35 -0500

    refactor(api): new api endpoint and key

--- src/api/aiApi.ts
+++ src/api/aiApi.ts
@@ -1,12 +1,15 @@
 import axios from "axios";
-const { VITE_AI_API_URL } = import.meta.env;
-const baseURL = `${VITE_AI_API_URL}`;
+const { VITE_AI_API_URL, VITE_API_KEY } = import.meta.env;
 
 const aiApi = axios.create({
-	baseURL,
-	headers: {
-		"Content-Type": "application/json",
-	},
+  baseURL: VITE_AI_API_URL,
+  params: {
+    model: "turbo",
+  },
+  headers: {
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${VITE_API_KEY}`,
+  },
 });
 
 export default aiApi;
